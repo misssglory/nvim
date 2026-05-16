574b730eeb5ada31c310aa3ed8c6eda6a49d6e79
Dmitriy L <xhaustlesss@gmail.com>
2026-05-16 04:34:02 +0300

    Try fix block commenting

--- lua/plugin-list.lua
+++ lua/plugin-list.lua
@@ -1,4 +1,4 @@
--- lua/plugin-list.lua - WITHOUT wff entry
+-- lua/plugin-list.lua
 return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons",
@@ -6,7 +6,7 @@ return {
     { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
     "nvim-treesitter/nvim-treesitter-context",
 
-    { "saghen/blink.cmp",     branch = "v1" },
+    { "saghen/blink.cmp", branch = "v1" },
 
     "nvim-telescope/telescope.nvim",
     { "ThePrimeagen/harpoon", branch = "harpoon2" },
@@ -19,6 +19,6 @@ return {
     "ojroques/vim-oscyank",
     "kkoomen/vim-doge",
     "captbaritone/better-indent-support-for-php-with-html",
-    "numToStr/Comment.nvim",
+    "tpope/vim-commentary",
     -- wff is not a plugin to clone - it's a system binary
 }
